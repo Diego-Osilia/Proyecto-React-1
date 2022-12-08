ef197a8e3458e14c463906709968a0d819d08185
Diego-Osilia <diegoalejandrosilia@gmail.com>
2022-12-07 21:31:13 -0300

    Mensajito

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>React app - This is my app</h1>
+        <p>VIVA PLATZIIII</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
